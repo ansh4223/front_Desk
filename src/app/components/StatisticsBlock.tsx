0808aedffac11ecde3b52008b51f7b525076a178
Himanshu Kumar <52701083+ansh4223@users.noreply.github.com>
2024-06-15 22:42:02 +0530

    Update StatisticsBlock.tsx

--- src/app/components/StatisticsBlock.tsx
+++ src/app/components/StatisticsBlock.tsx
@@ -6,7 +6,7 @@ type StatisticsBlockProps = {
 };
 
 const StatisticsBlock: React.FC<StatisticsBlockProps> = ({ label, value }) => (
-  <div className="flex-1 bg-gray-100 p-2 rounded shadow flex items-center justify-between">
+  <div className="flex-1 bg-white p-2 rounded shadow flex items-center justify-between">
     <div className="flex items-center">
       <span className="mr-2">{label}</span>
       <span className="text-gray-500">{value}</span>
